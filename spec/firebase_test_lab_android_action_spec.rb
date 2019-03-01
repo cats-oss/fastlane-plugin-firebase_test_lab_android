b153a14b1ffd12e23c44116a556988a7a146cfbe
wasabeef <dadadada.chop@gmail.com>
2019-03-01 14:33:02 +0900

    remove rspec

--- spec/firebase_test_lab_android_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::FirebaseTestLabAndroidAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The firebase_test_lab_android plugin is working!")
-
-      Fastlane::Actions::FirebaseTestLabAndroidAction.run(nil)
-    end
-  end
-end
