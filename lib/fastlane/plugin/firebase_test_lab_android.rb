4ccfc17c7a8e1ac552084c94720854f1607f2cc6
wasabeef <dadadada.chop@gmail.com>
2019-11-05 17:21:27 +0900

    add slack helper

--- lib/fastlane/plugin/firebase_test_lab_android.rb
+++ lib/fastlane/plugin/firebase_test_lab_android.rb
@@ -3,7 +3,7 @@ require 'fastlane/plugin/firebase_test_lab_android/version'
 module Fastlane
   module FirebaseTestLabAndroid
     def self.all_classes
-      Dir[File.expand_path('**/{actions}/*.rb', File.dirname(__FILE__))]
+      Dir[File.expand_path('**/{actions,helper}/**/*.rb', File.dirname(__FILE__))]
     end
   end
 end
